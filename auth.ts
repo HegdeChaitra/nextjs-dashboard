479e733db1cfc47afea6405413aff78d78b86f4a
HegdeChaitra <cvh255@nyu.edu>
2024-05-12 23:58:00 -0700

    handle null

--- auth.ts
+++ auth.ts
@@ -25,18 +25,15 @@ export const { auth, signIn, signOut } = NextAuth({
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
 
+          if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
-          return user
-
-//           if (parsedCredentials.success) {
-//           const { email, password } = parsedCredentials.data;
-//           const user = await getUser(email);
-//           if (!user) return null;
-//           const passwordsMatch = await bcrypt.compare(password, user.password);
-//           if (passwordsMatch) return user;
-//         }
-//         console.log('Invalid credentials');
-//         return null;
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        console.log('Invalid credentials');
+        return null;
       },
     }),
   ],
