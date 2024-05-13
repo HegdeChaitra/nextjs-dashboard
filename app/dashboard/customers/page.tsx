ae3e9fdb89f5ee0d1bd89303a94f7c65c34ac350
HegdeChaitra <cvh255@nyu.edu>
2024-05-12 22:30:34 -0700

    create customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,6 @@
 import Form from '@/app/ui/customers/create-customer';
-// import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-// import { fetchCustomers } from '@/app/lib/data';
 
 export default async function Page() {
-//   const customers = await fetchCustomers();
 
   return (
     <main>
